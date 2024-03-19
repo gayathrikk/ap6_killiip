6e943989680e4459c795eaa4fecf7264062e9f85
gayathrikk <gayathri@htic.iitm.ac.in>
2024-03-19 16:30:30 +0530

    first commit

---
+++ src/test/java/com/test/Database_Testing/public_currentdate_login.java
@@ -0,0 +1,39 @@
+package com.test.Database_Testing;
+import java.sql.*;
+
+import org.junit.Test;
+
+public class public_currentdate_login {
+	
+    Connection connection = null;
+
+    @Test
+    public void run() throws SQLException, ClassNotFoundException
+    {
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        connection = DriverManager.getConnection(
+            "jdbc:mysql://private.colab.humanbrain.in:3306/HBA_V2",
+            "root", "Health#123");
+            Statement statement;
+            statement = connection.createStatement();
+            ResultSet resultSet;
+            resultSet = statement.executeQuery(
+                "SELECT activity.user,CC_User.user_name,activity.timestamp FROM activity inner join CC_User on activity.user=CC_User.id where action=\"login\" and timestamp>=curdate()*1000000");
+            String user;
+            int userid;
+            String time;
+            String format = "%-7d%-30s%-10s%s%n";
+            String format2 = "%-7s%-30s%-10s%s%n";
+            System.out.printf(format2,"userid","User","action","Time");
+            while (resultSet.next()) {
+                userid = resultSet.getInt("user");
+                user = resultSet.getString("user_name").trim();
+                time = resultSet.getString("timestamp").trim();
+                System.out.printf(format, userid, user, "HD Viewer", time);
+            }
+            resultSet.close();
+            statement.close();
+            connection.close();
+    }
+
+}
