0439d13927be4444db92d0baf6755272e56d066a
gayathrikk <gayathri@htic.iitm.ac.in>
2024-06-05 13:16:52 +0530

    update commit

--- src/test/java/com/test/Database_Testing/QC_Pending_Batches.java
+++ src/test/java/com/test/Database_Testing/QC_Pending_Batches.java
@@ -1,66 +1,150 @@
 package com.test.Database_Testing;
 
+import org.testng.annotations.Test;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
-
-import org.testng.annotations.Test;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
+import java.util.Properties;
+import javax.mail.*;
+import javax.mail.internet.*;
+import javax.mail.internet.MimeMessage;
 
 public class QC_Pending_Batches {
-    
-    @Test
-    public void testDB() throws ClassNotFoundException, SQLException {
-        Class.forName("com.mysql.jdbc.Driver");
-        System.out.println("Driver loaded");
 
-        String url = "jdbc:mysql://apollo2.humanbrain.in:3306/HBA_V2";
-        String username = "root";
-        String password = "Health#123";
-        Connection connection = DriverManager.getConnection(url, username, password);
-        System.out.println("MYSQL database connected");
-        
-        executeAndPrintQuery(connection);
-        connection.close();
+    @Test
+    public void testDB() {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            System.out.println("Driver loaded");
+
+            String url = "jdbc:mysql://apollo2.humanbrain.in:3306/HBA_V2";
+            String username = "root";
+            String password = "Health#123";
+            Connection connection = DriverManager.getConnection(url, username, password);
+            System.out.println("MYSQL database connected");
+
+            executeAndPrintQuery(connection);
+            connection.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
-    private void executeAndPrintQuery(Connection connection) throws SQLException {
-        Statement statement = connection.createStatement();
-        String query1 = "SELECT id,name,datalocation,arrival_date,totalImages FROM `slidebatch` WHERE `process_status` = 6 AND `arrival_date` < DATE_SUB(NOW(), INTERVAL 4 DAY);";
+    private void executeAndPrintQuery(Connection connection) {
+        try {
+            Statement statement = connection.createStatement();
+            String query = "SELECT id, name, datalocation, arrival_date, totalImages " +
+                    "FROM `slidebatch` " +
+                    "WHERE (process_status = 6 OR process_status = 11) " +
+                    "AND `arrival_date` < CURDATE();";
 
-        ResultSet resultSet = statement.executeQuery(query1);
-        
-        int IdWidth = 10;
-        int nameWidth = 40;
-        int datalocationWidth = 30;
-        int arrival_dateWidth = 20;
-        int totalImagesWidth = 25;
+            ResultSet resultSet = statement.executeQuery(query);
 
-        // Printing header
-        System.out.printf("%-" + IdWidth + "s %-"+ nameWidth + "s %-"+ datalocationWidth + "s %-"+ arrival_dateWidth + "s %-" + totalImagesWidth + "s%n",
-                "Id", "name", "datalocation", "arrival_date", "totalImages");
+            int IdWidth = 10;
+            int nameWidth = 40;
+            int datalocationWidth = 30;
+            int arrival_dateWidth = 20;
+            int totalImagesWidth = 20;
+            int daysWidth = 15; // Width for the new column
 
-        // Printing separator line
-        String separatorLine = "-".repeat(IdWidth + nameWidth + datalocationWidth + arrival_dateWidth + totalImagesWidth);
-        System.out.println(separatorLine);
-        
-        while (resultSet.next()) {
-            int id = resultSet.getInt("id");
-            String name = resultSet.getString("name");
-            String datalocation = resultSet.getString("datalocation");
-            String arrival_date = resultSet.getString("arrival_date"); // Assuming arrival_date is stored as a String
-            int totalImages = resultSet.getInt("totalImages");
+            // Building email content
+            StringBuilder emailContent = new StringBuilder();
+            emailContent.append("<html><body><pre>");
+            emailContent.append("For your attention and action:\n\n");
+            emailContent.append(String.format("%-" + IdWidth + "s %-"+ nameWidth + "s %-"+ datalocationWidth + "s %-"+ arrival_dateWidth + "s %-" + totalImagesWidth + "s %-" + daysWidth + "s%n",
+                    "Id", "name", "datalocation", "arrival_date", "totalImages", "No.of days"));
 
-            System.out.printf("%-" + IdWidth + "d %-" + nameWidth + "s %-" + datalocationWidth + "s %-" + arrival_dateWidth + "s %-" + totalImagesWidth + "d%n",
-                   id, name, datalocation, arrival_date, totalImages);
+            // Adding separator line
+            String separatorLine = "-".repeat(IdWidth + nameWidth + datalocationWidth + arrival_dateWidth + totalImagesWidth + daysWidth);
+            emailContent.append(separatorLine).append("\n");
+
+            boolean dataFound = false;
+
+            while (resultSet.next()) {
+                dataFound = true;
+                int id = resultSet.getInt("id");
+                String name = resultSet.getString("name");
+                String datalocation = resultSet.getString("datalocation");
+                String arrivalDateStr = resultSet.getString("arrival_date"); // Assuming arrival_date is stored as a String
+                int totalImages = resultSet.getInt("totalImages");
+
+                LocalDate arrivalDate = LocalDate.parse(arrivalDateStr, DateTimeFormatter.ofPattern("yyyy-MM-dd"));
+                long daysDifference = ChronoUnit.DAYS.between(arrivalDate, LocalDate.now());
+
+                emailContent.append(String.format("%-" + IdWidth + "d %-" + nameWidth + "s %-" + datalocationWidth + "s %-" + arrival_dateWidth + "s %-" + totalImagesWidth + "d %-" + daysWidth + "d%n",
+                        id, name, datalocation, arrivalDateStr, totalImages, daysDifference));
+            }
+
+            emailContent.append("</pre></body></html>");
+
+            // Close the statement
+            resultSet.close();
+            statement.close();
+
+            if (dataFound) {
+                sendEmailAlert(emailContent.toString());
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
         }
+    }
 
-        // Close the statement
-        resultSet.close();
-        statement.close();
+    private void sendEmailAlert(String messageBody) {
+        // Recipient's email ID needs to be mentioned.
+        String to = "gayathrigayu0918@gmail.com";
+
+        // Sender's email ID needs to be mentioned
+        String from = "gayathri@htic.iitm.ac.in";
+
+        // Assuming you are sending email from through gmails smtp
+        String host = "smtp.gmail.com";
+
+        // Get system properties
+        Properties properties = System.getProperties();
+
+        // Setup mail server
+        properties.put("mail.smtp.host", host);
+        properties.put("mail.smtp.port", "465");
+        properties.put("mail.smtp.ssl.enable", "true");
+        properties.put("mail.smtp.auth", "true");
+
+        // Get the Session object and pass username and password
+        Session session = Session.getInstance(properties, new javax.mail.Authenticator() {
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication("gayathri@htic.iitm.ac.in", "Gayu@0918");
+            }
+        });
+
+        // Used to debug SMTP issues
+        session.setDebug(true);
+
+        try {
+            // Create a default MimeMessage object.
+            MimeMessage message = new MimeMessage(session);
+
+            // Set From: header field of the header.
+            message.setFrom(new InternetAddress(from));
+
+            // Set To: header field of the header.
+            message.addRecipient(Message.RecipientType.TO, new InternetAddress(to));
+
+            // Set Subject: header field
+            message.setSubject("Scanning Pipeline: Image QC: Alert");
+
+            // Set the actual message
+            message.setContent(messageBody, "text/html");
+
+            System.out.println("sending...");
+            // Send message
+            Transport.send(message);
+            System.out.println("Sent message successfully....");
+        } catch (MessagingException mex) {
+            mex.printStackTrace();
+        }
     }
 }
-
-
-
